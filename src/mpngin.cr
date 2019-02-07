ee12f131b6814c19f4c3bd29dbf9e2b2c89808cf
Adam Radabaugh <adam@walkingtoast.com>
2019-02-06 22:26:53 -0600

    Run formatter.

--- src/mpngin.cr
+++ src/mpngin.cr
@@ -9,9 +9,9 @@ Dotenv.load
 
 module Mpngin
   FALLBACK_URL = ENV["FALLBACK_URL"].to_s
-  REDIS_DB = ENV["REDIS_DATABASE"].to_i
+  REDIS_DB     = ENV["REDIS_DATABASE"].to_i
   SECRET_TOKEN = ENV["SECRET_TOKEN"].to_s
-  SHORT_URL = ENV["SHORT_URL"].to_s
+  SHORT_URL    = ENV["SHORT_URL"].to_s
 
   class Auth
     def self.authorized?(request)
