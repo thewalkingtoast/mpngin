d9786f7496d7b53d1b3b56ed9b94c96847f27e67
Adam Radabaugh <adam@walkingtoast.com>
2020-06-05 17:36:31 -0700

    Skip crystal-coverage due to false positives

--- src/mpngin/constants.cr
+++ src/mpngin/constants.cr
@@ -4,4 +4,5 @@ module Mpngin
   SECRET_TOKEN         = ENV["SECRET_TOKEN"].to_s
   SHORT_URL            = ENV["SHORT_URL"].to_s
   PORT                 = ENV.fetch("PORT", "3000").to_i
+  LINK_REPORT_CSV_NAME = ENV.fetch("LINK_REPORT_CSV_NAME", "MPNGIN_Link_Report")
 end
